5f838a4359b1b12ddea0be9054cceeef335ea7b8
Jacob Dunbar <jacobfdunbar@gmail.com>
2022-07-26 15:41:44 -0400

    Updating ManagedObject.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 
 ==
+## [1.0.2] - 2022-07-26
+### Changed
+- ManagedObject now only uses OnEnable and OnDisable lifetime methods.
 
 ## [1.0.1] - 2022-06-29
 ### Added
--- Runtime/ManagedObject.cs
+++ Runtime/ManagedObject.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace GI.UnityToolkit.Variables
 {
@@ -9,36 +7,6 @@ namespace GI.UnityToolkit.Variables
         protected abstract void OnBegin();
         protected abstract void OnEnd();
 
-#if UNITY_EDITOR
-        protected void OnEnable()
-        {
-            EditorApplication.playModeStateChanged += OnPlayStateChange;
-        }
-
-        protected void OnDisable()
-        {
-            EditorApplication.playModeStateChanged -= OnPlayStateChange;
-        }
-
-        private void OnPlayStateChange(PlayModeStateChange state)
-        {
-            switch (state)
-            {
-                case PlayModeStateChange.EnteredPlayMode:
-                    OnBegin();
-                    break;
-                case PlayModeStateChange.ExitingPlayMode:
-                    OnEnd();
-                    break;
-                case PlayModeStateChange.EnteredEditMode:
-                    break;
-                case PlayModeStateChange.ExitingEditMode:
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
-            }
-        }
-#else
         protected void OnEnable()
         {
             OnBegin();
@@ -48,6 +16,5 @@ namespace GI.UnityToolkit.Variables
         {
             OnEnd();
         }
-#endif
     }
 }
