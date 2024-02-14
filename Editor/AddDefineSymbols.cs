7180f159f09455b7e108c3e9623c0a1d62598cac
Jacob Dunbar <jacobfdunbar@gmail.com>
2024-02-14 17:02:01 -0500

    Fixing define symbols again.

--- Editor/AddDefineSymbols.cs
+++ Editor/AddDefineSymbols.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using UnityEditor;
 using UnityEditor.Build;
+using UnityEditor.Compilation;
 
 namespace GI.UnityToolkit.Variables.Editor
 {
@@ -18,9 +19,15 @@ namespace GI.UnityToolkit.Variables.Editor
         {
             "GI_VARIABLES"
         };
-
+        
         public int callbackOrder => 0;
 
+        static AddDefineSymbols()
+        {
+            CompilationPipeline.compilationStarted += ProcessBatchModeCompileFinish;
+        }
+
+        private static void ProcessBatchModeCompileFinish(object obj) => AddDefinesAsNeeded();
         private void OnPreprocessAsset() => AddDefinesAsNeeded();
         public void OnActiveBuildTargetChanged(BuildTarget previousTarget, BuildTarget newTarget) => AddDefinesAsNeeded();
 
